31627649ef5a4ccb42062c68765be6c1e5970789
Richard Marcinčák <55552821+Em1tt@users.noreply.github.com>
2022-06-06 03:33:08 +0200

    Add section

--- high-contrast.js
+++ high-contrast.js
@@ -2,10 +2,15 @@ function toggle(){
     document.body.classList.toggle("hc");
 }
 
+const toggleButton = document.querySelector("#hcToggle");
+
 document.addEventListener("keypress", (event) => {
-    console.log(event);
     if(event.key.toLowerCase() == "a" && event.shiftKey){
-        console.log("hi");
+        toggleButton.checked = toggleButton.checked ? false : true;
         toggle();
     }
 });
+
+toggleButton.addEventListener("change", (event) => {
+    toggle();
+})
