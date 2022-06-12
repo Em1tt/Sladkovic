81500180c1b8127d65b268002e1e00e559798732
Richard Marcinčák <55552821+Em1tt@users.noreply.github.com>
2022-06-12 22:38:12 +0200

    New Section

--- animate.js
+++ animate.js
@@ -15,4 +15,44 @@
             element.classList.add("slide-in-right");
         }, parseInt(delay));
     }, false);
-})
+});
+[...document.querySelectorAll(".animate-bottom")].forEach(element => {
+    const delay = element.getAttribute("data-delay");
+    //Wait til load
+    document.addEventListener('DOMContentLoaded', () => {
+        setTimeout(() => {
+            element.classList.add("slide-in-bottom");
+        }, parseInt(delay));
+    }, false);
+});
+(() => {
+    document.addEventListener("scroll", (event) => {
+        const contents = [...document.querySelectorAll(".container")];
+        for(element of contents){
+            if(isInMiddle(element) && !isPastMiddle(element)){
+                if(element.id && !element.classList.contains("activated")){
+                    const icon = document.getElementById("fa-"+element.id);
+                    icon.classList.add("shown");
+                }
+                element.classList.add("activated");
+            }else{
+                try{
+                    const icon = document.getElementById("fa-"+element.id);
+                    icon.classList.remove("shown");
+                }catch(e){
+                    e;
+                }
+                element.classList.remove("activated");
+            }
+        }
+    });
+})();
+
+function isInMiddle(element) {
+    const rect = element.getBoundingClientRect();
+    return window.innerHeight/2+parseInt(element.clientHeight)/2 > parseInt(rect.top);
+}
+function isPastMiddle(element) {
+    const rect = element.getBoundingClientRect();
+    return window.innerHeight/2-parseInt(element.clientHeight)/2 > parseInt(rect.top);
+}
